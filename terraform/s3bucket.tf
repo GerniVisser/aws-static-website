4f7db513c3f65a5f32adb19efd59f1a35f80b5ef
GerniVisser-bbd <Gerrit.Visser@bbd.co.za>
2024-09-24 13:20:25 +0200

    removed bucket random postfix

--- terraform/s3bucket.tf
+++ terraform/s3bucket.tf
@@ -1,13 +1,6 @@
-resource "random_string" "bucket_postfix" {
-  length  = 4
-  special = false
-  upper = false
-}
-
 resource "aws_s3_bucket" "bucket-root" {
-  bucket = "root.${var.bucket_name}.${random_string.bucket_postfix.result}"
+  bucket = "root.${var.bucket_name}"
   force_destroy = true
-  depends_on = [ random_string.bucket_postfix ]
 }
 
 data "aws_s3_bucket" "root-bucket" {
@@ -15,9 +8,8 @@ data "aws_s3_bucket" "root-bucket" {
 }
 
 resource "aws_s3_bucket" "bucket-log" {
-  bucket = "log.${var.bucket_name}-${random_string.bucket_postfix.result}"
+  bucket = "log.${var.bucket_name}"
   force_destroy = true
-  depends_on = [ random_string.bucket_postfix ]
 }
 
 data "aws_s3_bucket" "log-bucket" {
